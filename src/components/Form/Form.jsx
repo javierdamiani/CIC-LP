8358528320793c7a92cd2506c125b2e73d483f83
javierdamiani <javierdamiani74@gmail.com>
2024-07-25 14:12:00 -0500

    form responsive

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -28,7 +28,7 @@ function Form() {
 
   return (
     <form
-      className=" flex flex-col gap-y-2 sm:max-h-[400px] mx-auto bg-gray-50 rounded shadow-md p-2"
+      className="flex flex-col mx-auto bg-gray-50 rounded shadow-md p-4 sm:p-10 gap-1 justify-center w-full max-w-lg"
       onSubmit={handleSubmit}
     >
       <h3 className="text-sm">Registra tus datos y recibe más información</h3>
@@ -48,71 +48,78 @@ function Form() {
           <option value="familiar">Familiar</option>
         </select>
       </div>
-      <div className="text-sm w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+      <div className="w-full">
         <input
           type="text"
           placeholder="Nombre"
           name="name"
           onChange={handleChange}
           value={data.name}
+          className="mt-1 text-sm block w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
         />
       </div>
-      <div className="text-sm w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+      <div className="w-full">
         <input
           type="text"
           placeholder="Apellido"
           name="lastname"
           onChange={handleChange}
           value={data.lastname}
+          className="mt-1 text-sm block w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
         />
       </div>
-      <div className="flex flex-row gap-x-2">
-        <div className="text-sm w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+      <div className="flex flex-col sm:flex-row gap-2 w-full">
+        <div className="w-full">
           <input
             type="text"
             placeholder="Apellido Paterno"
             name="fatherlastname"
             onChange={handleChange}
             value={data.fatherlastname}
+            className="mt-1 text-sm block w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
           />
         </div>
-        <div className="text-sm w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+        <div className="w-full">
           <input
             type="text"
             placeholder="Apellido Materno"
             name="motherlastname"
             onChange={handleChange}
             value={data.motherlastname}
+            className="mt-1 text-sm block w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
           />
         </div>
       </div>
-      <div className="flex flex-row gap-x-2">
-        <div className="text-sm w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+      <div className="flex flex-col sm:flex-row gap-2 w-full">
+        <div className="w-full">
           <input
             type="number"
             placeholder="Celular"
             name="cel"
             onChange={handleChange}
             value={data.cel}
+            className="mt-1 text-sm block w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
           />
         </div>
-        <div className="text-sm w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+        <div className="w-full">
           <input
             type="text"
             placeholder="Correo electronico"
             name="email"
             onChange={handleChange}
             value={data.email}
+            className="mt-1 text-sm block w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
           />
         </div>
       </div>
-      <div className="text-sm w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+      <div className="w-full">
         <input
           type="text"
           placeholder="Colegio de procedencia"
           name="school"
           onChange={handleChange}
           value={data.school}
+          className="mt-1 text-sm block w-full bg-white border border-gray-300 rounded py-1 px-2 shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
         />
       </div>
       <div>
@@ -132,16 +139,16 @@ function Form() {
           <option value="whatsapp">Whatsapp</option>
         </select>
       </div>
-      <div className="flex flex-row gap-x-2">
+      <div className="flex items-start gap-2 mt-4">
         <input
           type="checkbox"
           name="checkbox"
           onChange={handleChange}
           value={data.checkbox}
         />
-        <div>
-          <p className=" text-xs">
-            Otorgo mi consetimiento para que mis datos sean tratados segun la
+        <div className="text-xs">
+          <p>
+            Otorgo mi consentimiento para que mis datos sean tratados según la
           </p>
           <a
             href="https://www.google.com"
@@ -149,13 +156,12 @@ function Form() {
             rel="noopener noreferrer"
             className="text-xs text-[#7d13eF]"
           >
-            Politica de tratamiento de datos
+            Política de tratamiento de datos
           </a>
         </div>
       </div>
       <button
-        className="bg-[#7d13eF] hover: hover:bg-[#56347c] text-white font-bold py-2 px-4 rounded-3xl"
-        // disabled={!data.checkbox}
+        className="bg-[#7d13eF] hover:bg-[#56347c] text-white font-bold py-2 px-4 rounded-3xl mt-4"
         type="submit"
       >
         Enviar
